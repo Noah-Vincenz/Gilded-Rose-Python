b147bb0d01236b0b3cd8ea30449d19dd30439a7d
Noah-Vincenz Noeh <NoahVincenz.Noeh@aexp.com>
2020-08-20 12:52:53 +0100

    Some more refactoring.

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -1,3 +1,11 @@
+def decrement_quality(item): 
+    if item.quality > 0:
+        item.quality -= 1
+
+def increment_quality(item): 
+    if item.quality < 50:
+        item.quality += 1
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -6,26 +14,23 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             name = item.name
-            if (name != "Aged Brie" and name != "Backstage passes to a TAFKAL80ETC concert" and name != "Sulfuras, Hand of Ragnaros") and (item.quality > 0):
-                item.quality -= 1
+            if name != "Aged Brie" and name != "Backstage passes to a TAFKAL80ETC concert" and name != "Sulfuras, Hand of Ragnaros":
+                decrement_quality(item)
             elif item.quality < 50:
                 item.quality += 1
                 if name == "Backstage passes to a TAFKAL80ETC concert":
                     if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality += 1
+                        increment_quality(item)
                     if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality += 1
+                        increment_quality(item)
             if name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in -= 1
             if item.sell_in < 0:
                 if name != "Aged Brie":
                     if name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if name != "Sulfuras, Hand of Ragnaros":
-                                item.quality -= 1
+                        if name != "Sulfuras, Hand of Ragnaros":
+                            decrement_quality(item)
                     else:
                         item.quality = 0
-                elif item.quality < 50:
-                    item.quality += 1
+                else:
+                    increment_quality(item)
