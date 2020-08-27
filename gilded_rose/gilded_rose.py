7e0c910b6ba4bfb82ed2b77429d50c681f5be356
Noah-Vincenz Noeh <NoahVincenz.Noeh@aexp.com>
2020-08-27 15:59:57 +0100

    Added constants.

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -8,17 +8,21 @@ def increment_quality(item):
 
 class GildedRose(object):
 
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
+    BRIE = "Aged Brie"
+    BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
             name = item.name
-            if name != "Aged Brie" and name != "Backstage passes to a TAFKAL80ETC concert" and name != "Sulfuras, Hand of Ragnaros":
+            if name != GildedRose.BRIE and name != GildedRose.BACKSTAGE and name != GildedRose.SULFURAS:
                 decrement_quality(item)
             elif item.quality < 50:
                 item.quality += 1
-                if name == "Backstage passes to a TAFKAL80ETC concert":
+                if name == GildedRose.BACKSTAGE:
                     if item.sell_in < 11:
                         increment_quality(item)
                     if item.sell_in < 6:
@@ -26,9 +30,9 @@ class GildedRose(object):
             if name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in -= 1
             if item.sell_in < 0:
-                if name != "Aged Brie":
-                    if name != "Backstage passes to a TAFKAL80ETC concert":
-                        if name != "Sulfuras, Hand of Ragnaros":
+                if name != GildedRose.BRIE:
+                    if name != GildedRose.BACKSTAGE:
+                        if name != GildedRose.SULFURAS:
                             decrement_quality(item)
                     else:
                         item.quality = 0
