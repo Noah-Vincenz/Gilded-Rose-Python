1658676452718fd2f3aafb8f096d19b1708ef045
Noah-Vincenz Noeh <NoahVincenz.Noeh@aexp.com>
2020-08-28 13:32:32 +0100

    Moved functions inside class.

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -1,11 +1,3 @@
-def decrement_quality(item): 
-    if item.quality > 0:
-        item.quality -= 1
-
-def increment_quality(item): 
-    if item.quality < 50:
-        item.quality += 1
-
 class GildedRose(object):
 
     SULFURAS = "Sulfuras, Hand of Ragnaros"
@@ -19,22 +11,30 @@ class GildedRose(object):
         for item in self.items:
             name = item.name
             if name != GildedRose.BRIE and name != GildedRose.BACKSTAGE and name != GildedRose.SULFURAS:
-                decrement_quality(item)
+                self.decrement_quality(item)
             elif item.quality < 50:
                 item.quality += 1
                 if name == GildedRose.BACKSTAGE:
                     if item.sell_in < 11:
-                        increment_quality(item)
+                        self.increment_quality(item)
                     if item.sell_in < 6:
-                        increment_quality(item)
-            if name != "Sulfuras, Hand of Ragnaros":
+                        self.increment_quality(item)
+            if name != GildedRose.SULFURAS:
                 item.sell_in -= 1
             if item.sell_in < 0:
                 if name != GildedRose.BRIE:
                     if name != GildedRose.BACKSTAGE:
                         if name != GildedRose.SULFURAS:
-                            decrement_quality(item)
+                            self.decrement_quality(item)
                     else:
                         item.quality = 0
                 else:
-                    increment_quality(item)
+                    self.increment_quality(item)
+
+    def decrement_quality(self, item): 
+        if item.quality > 0:
+            item.quality -= 1
+
+    def increment_quality(self, item): 
+        if item.quality < 50:
+            item.quality += 1
